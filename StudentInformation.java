a00406fd26335721df7f17212dc5d24839ea6407
apcompZ <71896832+apcompZ@users.noreply.github.com>
2020-10-02 15:37:44 -0400

    Add files via upload

---
+++ StudentInformation.java
@@ -0,0 +1,19 @@
+
+public class StudentInformation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("my name is zahra");
+		System.out.println("my favourite colour is grey");
+		System.out.println("I am sixteen years old");
+		System.out.println("my favourite class is science");
+		System.out.println("I like to cook");
+		System.out.println("I'm 5 foot 7 inches tall");
+		System.out.println("my Favourrite food is mango frozen yoguart");
+		System.out.println("I hate to read");
+		System.out.println("I have a cat");
+		System.out.println("I play Softball");
+		System.out.println("my favourtie show is Lucifer");
+	}
+
+}
